f23d02feeb5d903bf393cf01102d7901fa6286dd
Mark Hanna <cipscis+github@gmail.com>
2019-03-15 14:56:46 +1300

    Changed autocomplete from a POST to a GET so it will work on Github pages

--- server/server.js
+++ server/server.js
@@ -3,10 +3,4 @@ var app = express();
 
 app.use(express.static('.'));
 
-// If the app receives a POST with a URL ending in 'static.json', return the file
-app.post(/static\.json$/, function (req, res) {
-	var filePath = './' + req.url;
-	res.download(filePath);
-});
-
 app.listen(8080, function () {});

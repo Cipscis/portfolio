3b7b3ea72103aadc4628cb39b298442d1d6b6f98
Brooke Hart <brooke+github@curly.kiwi>
2019-03-20 14:26:06 +1300

    Tidied incomplete validation section on web forms reference page, and added JS for it

---
+++ assets/scripts/validate.js
@@ -0,0 +1,53 @@
+const validate = (function (throttle) {
+	const selectors = {
+		invalid: 'input:invalid, select:invalid, textarea:invalid',
+		status: '.js-validate-status'
+	}
+
+	const classes = {
+		validated: 'is-validated',
+
+		error: 'is-error'
+	};
+
+	const module = {
+		bindValidateEvent: function ($forms) {
+			if (typeof $forms === 'string') {
+				$forms = document.querySelectorAll($forms);
+			} else if (!Array.isArray($forms)) {
+				$forms = [$forms];
+			}
+
+			for (let i = 0; i < $forms.length; i++) {
+				let $form = $forms[i];
+
+				// Throttling at such a tiny delay prevents multiple concurrent calls
+				$form.addEventListener('invalid', throttle(module._invalidEvent, 10), true);
+			}
+		},
+
+		_invalidEvent: function (e) {
+			var $form = e.target.form;
+			var $invalidInputs = $form.querySelectorAll(selectors.invalid);
+			var $status = $form.querySelectorAll(selectors.status);
+
+			$form.classList.add(classes.validated);
+
+			$status.forEach(($status) => {
+				$status.classList.add(classes.error);
+				$status.textContent = '';
+
+				// Set asynchronously so the new text will be read by screen readers
+				window.setTimeout(() => $status.textContent = `We couldn't submit the form because of ${$invalidInputs.length} invalid input${$invalidInputs.length > 1 ? 's' : ''}.`, 100);
+			});
+		},
+
+		_clearStatusClass: function ($form) {
+			var $status = $form.querySelectorAll(selectors.status);
+
+			$status.foreach(($status) => $status.classList.remove(classes.error));
+		}
+	};
+
+	return module.bindValidateEvent;
+})(throttle);
